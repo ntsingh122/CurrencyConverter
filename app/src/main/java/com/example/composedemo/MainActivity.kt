4350e58a515f87c94897df54dd315ed4cb732049
n0c0clx <Nitin.chaudhary@walmart.com>
2026-03-21 23:10:28 +0530

    fix: remove unnecessary code

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -4,32 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -39,65 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeDemoTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    FeedScreen({ callCoroutine() })
-                }
             }
         }
     }
 
-    suspend fun callCoroutine() {
-        coroutineScope { }
-        lifecycleScope.launch {
-            val result = withContext(Dispatchers.Main) {
-                delay(11000)
-                print("delayed")
-                24
-            }
-        }
-    }
-}
-
-
-@Composable
-fun FeedScreen(callCoroutine: suspend () -> Unit) {
-    // 1. We create a state object to track the scroll position of the list
-    val listState = rememberLazyListState()
-    LaunchedEffect(Unit) {
-        callCoroutine.invoke()
-    }
-
-    val showButton by remember {
-        derivedStateOf {
-            listState.firstVisibleItemIndex > 0
-        }
-    }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        // 3. The List
-        LazyColumn(
-            state = listState,
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            items(100) { index ->
-                Text(
-                    text = "Item #$index",
-                    modifier = Modifier.padding(vertical = 12.dp)
-                )
-            }
-        }
-
-        // 4. The Button
-        AnimatedVisibility(
-            visible = showButton,
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ) {
-            FloatingActionButton(onClick = { /* Scroll to top logic later */ }) {
-                Text("Up")
-            }
-        }
-    }
 }
